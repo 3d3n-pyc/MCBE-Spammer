6df330525b4ac3e5b057cffc890c178d69c5da4e
LocheMan <96348438+LocheMan@users.noreply.github.com>
2024-01-13 15:14:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const ip = 'ploto.mcbe.fr';
 const port = 19132;
 
-const message = '§r§bAlexandre Warscotte §8= §aNagato §7?' // '§r§bVous souhaitez des spams efficaces ? §7-> §ahttps://discord.gg/NTW4xJ7UkU §8| §r§43D3N PRIME ?'
+const message = '§r§43D3N PRIME ?'
 const delay = 10
 
 const showRafales = true
@@ -10,7 +10,6 @@ const bedrock = require('bedrock-protocol');
 const axios = require('axios');
 
 const usernames = ['EdenAuPrime6594', 'EdenAuPrime5973', 'EdenAuPrime3907', 'EdenAuPrime6038', 'EdenAuPrime']
-// const usernames = ['CreakyShoe9617', 'ColderWolf4085', 'SneakNote8967', 'ModernFire8804', 'EvilChair456742', 'BonStream', 'SorvekVl', 'JeFeSaPrLaPatri', 'OvalAlpaca26776', 'WiseSeeker49675', 'SonicCorgi70177', 'U r Bottom', 'BOT2343899', 'ValuedTwo947073', 'FlexedLion7481', 'FieryCobra8025', 'VampyPond5855', 'ShellCoder8247', 'EarnedBull1607', 'JeTeDetruit1956'];
 
 for (let i = 0; i < usernames.length; i++) {
   let client;
